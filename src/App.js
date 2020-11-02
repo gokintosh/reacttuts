51dfe27e32b10c3cd266725591cb74d47e04285e
GOKUL KANNAMBALLY BALACHNADRAN <52125327+gokintosh@users.noreply.github.com>
2020-11-02 13:09:24 +0100

    add addfunction

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
 import Todo from "./components/Todo";
 import FilterButton from "./components/FilterButton";
 import Form from "./components/Form";
+import React,{useState} from "react";
+import {nanoid} from "nanoid";
+
+
+
+
+
+
+
+
+
 
 function App(props) {
-  const taskList=props.tasks.map(task=>(
+  const [tasks,setTasks]=useState(props.tasks);
+  function addTask(name){
+  const newTask={id:"todo"+nanoid(),name:name,completed:false};
+  setTasks([...tasks,newTask]);
+}
+  const taskList=tasks.map(task=>(
     <Todo id={task.id} name={task.name} completed={task.completed} key={task.id}/>
   ))
+  const taskNoun=taskList.length !==1?'tasks':'task';
+  const headingText=`${taskList.length} ${taskNoun} remaining`;
   return (
     <div className="todoapp stack-large">
       <h1>ToDoDo</h1>
-      <Form />
+      <Form addTask={addTask}/>
       <div className="filters btn-group stack-exception">
         <FilterButton/>
         <FilterButton/>
         <FilterButton/>
       </div>
       <h2 id="list-heading">
-        3 tasks remaining
+        {headingText}
       </h2>
       <ul
         role="list"
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React,{useState} from "react";
+
+
 
 
 function Form(props){
+    const [name,setName]=useState('');
+    function handleChange(e){
+        setName(e.target.value);
+    }
+    function handleSubmit(e){
+        e.preventDefault();
+        props.addTask(name);
+        setName("");
+    }
     return(
-        <form action="">
+        <form action="" onSubmit={handleSubmit}>
             <h2 className="label-wrapper">
                 <label htmlFor="new-todo-input" className="label__lg">
                     What needs to be done?
@@ -12,9 +23,11 @@ function Form(props){
             <input 
                 type="text"
                 id="new-todo-input"
+                value={name}
                 className="input input__lg"
                 name="text"
                 autoComplete="off"
+                onChange={handleChange}
             />
             <button type="submit" className="btn btn__primary btn__lg">
                 Add
